2f33da98fbba76ca09da0770ad3818e573805589
Michha02 <michha02@gmail.com>
2018-10-08 19:13:41 +0100

    Versión 5

--- src/Kata2.java
+++ src/Kata2.java
@@ -2,9 +2,10 @@ import java.util.Map;
 
 public class Kata2 {
     public static void main (String[] args){
-        Integer[] data = {0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9};
+        String[] data = {"Azul", "Verde", "Amarillo", "Rojo", "Naranja", "Blanco", "Negro", "Verde", "Verde",
+                         "Amarillo", "Rojo",};
         Histogram histo = new Histogram(data);
-        Map<Integer, Integer> histogr = histo.getHistogram();
+        Map<String, Integer> histogr = histo.getHistogram();
 
         histogr.forEach((k,v) -> System.out.println(k + " ==> " + v));
     }
